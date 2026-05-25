e10b1536be4d6cab96d68f570a47e63d8e8b69cb
slimaninino <slimaninasro08@gmail.com>
2026-05-25 20:40:18 +0200

    updates

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
-  readonly formAction = 'https://submit-form.com/9Q29PPSTH';
+export class ContactFormComponent {
+  submitted = false;
+  sending = false;
+
+  readonly googleFormAction =
+    'https://docs.google.com/forms/d/e/1FAIpQLSchCRl788Zfx7l0VUGpm6-Yvmo0m0yUeQ1bZvUBfdWvkcRC-A/formResponse';
+
+  form = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', [Validators.required]],
+    phone: [''],
+    comments: ['']
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.sending = true;
+    this.submitted = false;
+    setTimeout(() => {
+      this.sending = false;
+      this.submitted = true;
+      this.form.reset();
+    }, 500);
+  }
 }
